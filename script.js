fb800fc5e17c8357baa6ef672235cf1411aa313d
khhhrystyyna <khrystyna.styslo@gmail.com>
2022-02-09 22:38:44 +0200

    js base

---
+++ script.js
@@ -0,0 +1,54 @@
+// String
+let name = "Khrystyna"
+// Number
+const years = 21
+// Boolean
+const teacher = true
+
+const numberToString = 2022
+const stringToNumber = 2022
+
+const hello = "Hello "
+const world = "world "
+
+const sum = 23 + 34
+
+const number1 = 22
+const number2 = "5"
+
+console.log('hello world', hello + world)
+
+console.log(Number(numberToString))
+
+console.log('types number', typeof parseInt(numberToString))
+console.log('types string', typeof String(stringToNumber))
+console.log('name 1', name)
+
+
+console.log('number %%%%', numberToString === stringToNumber)
+
+
+
+name = 22
+console.log('name 2', name)
+// Copy variable
+const copyYears = years
+
+// console.log('years', years, copyYears)
+
+//
+// console.log(typeof name)
+// console.log(typeof years)
+// console.log(typeof teacher)
+
+const year2022 = 2022
+const year_2022 = 2022
+
+// @, -, /, #, . - не можна використовувати в назві змінних
+const Country = "Ukraine"
+const country = "Ukraine"
+
+const type_of_year = ""
+const nameName = ""
+
+
